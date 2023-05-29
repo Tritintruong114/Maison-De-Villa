ab5f4c65db3f603dd42f75d4741684f44a7f4bc2
Tritintruong114 <truongtritin.bee@gmail.com>
2023-05-29 17:04:20 +0700

    Stripe npm

--- src/pages/checkout/CheckOut.jsx
+++ src/pages/checkout/CheckOut.jsx
@@ -1,3 +1,8 @@
+import { loadStripe } from "@stripe/stripe-js";
+const stripe = await loadStripe(
+  "pk_test_51ND2G5Bhab7cMOe0DlfZhtQWqsc2XMnENUmGuX0d8jhar4r19QHEiBa8VHSExqxtd77HuwZIt00tVmNkK55m9qRL00aAmqkRK0"
+);
+
 const CheckOut = () => {
   return <div>CheckOut</div>;
 };
