d7dcc254eb1eda726dad4e8f263069c22da23f0c
bruno <truongtritin.bee@gmail.com>
2023-05-30 19:26:10 +0700

    Ok for mainpage

--- src/pages/mainpage/MainPage.jsx
+++ src/pages/mainpage/MainPage.jsx
@@ -1,5 +1,18 @@
+import homeimg from "./homeimg.jpg";
 const MainPage = () => {
-  return <div>MainPage</div>;
+  return (
+    <div className="font-poppins p-6 bg-darkBrown h-full">
+      <div className="sm:w-full w-full relative h-3/4 ">
+        <img
+          className=" rounded-3xl absolute h-full w-full object-cover"
+          src={homeimg}
+        />
+        <h1 className=" absolute sm:bottom-1/3 sm:ml-12 bottom-1/2 m-0 text-white text-6xl md:text-6xl">
+          Luxury <br /> Real Estate
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default MainPage;
