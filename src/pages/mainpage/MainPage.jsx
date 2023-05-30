0d04afde4770d35fb1d41d67d58732329ff96523
bruno <truongtritin.bee@gmail.com>
2023-05-31 01:46:06 +0700

    This is for main Ui

--- src/pages/mainpage/MainPage.jsx
+++ src/pages/mainpage/MainPage.jsx
@@ -1,15 +1,42 @@
+import { Link } from "react-router-dom";
 import homeimg from "./homeimg.jpg";
+import img2 from "./img2.jpg";
 const MainPage = () => {
   return (
-    <div className="font-poppins p-6 bg-darkBrown h-full">
-      <div className="sm:w-full w-full relative h-3/4 ">
-        <img
-          className=" rounded-3xl absolute h-full w-full object-cover"
-          src={homeimg}
-        />
-        <h1 className=" absolute sm:bottom-1/3 sm:ml-12 bottom-1/2 m-0 text-white text-6xl md:text-6xl">
-          Luxury <br /> Real Estate
-        </h1>
+    <div className="font-poppins overflow-scroll no-scrollbar gap-12 flex flex-col p-6 bg-darkBrown h-full">
+      <div className="sm:w-full flex-shrink-0 w-full relative h-3/4 ">
+        <Link to="uniquestays/black-house-in-the-woods">
+          <img
+            className=" rounded-3xl absolute h-full w-full object-cover"
+            src={homeimg}
+          />
+          <h1 className=" absolute sm:bottom-1/3 sm:ml-12 font-bold bottom-1/2 m-0 text-white text-6xl md:text-6xl">
+            Luxury <br /> Real Estate
+          </h1>
+        </Link>
+      </div>{" "}
+      <div className="sm:w-full  flex-shrink-0 w-full relative h-3/4 ">
+        <Link to="uniquestays/reza-villa">
+          <img
+            className=" rounded-3xl absolute h-full w-full object-cover"
+            src={img2}
+          />
+          <h1 className=" absolute sm:bottom-1/3 sm:ml-12 font-bold bottom-1/2 m-0 right-12 text-white text-6xl md:text-6xl">
+            Back to <br />
+            nature
+          </h1>
+        </Link>
+      </div>
+      <div className="sm:w-full  flex-shrink-0 w-full relative h-3/4 ">
+        <Link>
+          <img
+            className=" rounded-3xl absolute h-full w-full object-cover"
+            src={homeimg}
+          />
+          <h1 className=" absolute sm:bottom-1/3 sm:ml-12 bottom-1/2 m-0 text-white text-6xl md:text-6xl">
+            Luxury <br /> Real Estate
+          </h1>
+        </Link>
       </div>
     </div>
   );
