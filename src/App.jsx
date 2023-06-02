f5ee3c2f6485b7ed8813e1f19e5a0568a64e3392
bruno <truongtritin.bee@gmail.com>
2023-06-02 19:58:40 +0700

    Change back to maison - no chat

--- src/App.jsx
+++ src/App.jsx
@@ -12,19 +12,19 @@ function App() {
 
   return (
     <div className="flex no-scrollbar bg-opacity-60 bg-darkBrown overflow-hidden h-screen flex-col items-center">
-      {/* <div className="text-black sm:mt-9 sm:py-6 bg-white w-full bg-slate-400 max-w-screen-2xl">
+      <div className="text-black sm:mt-9 sm:py-6 bg-white w-full bg-slate-400 max-w-screen-2xl">
         <TopNavBar />
       </div>
       <div className="text-black overflow-scroll no-scrollbar h-screen bg-white w-full bg-slate-400 max-w-screen-2xl">
         <Outlet />
-      </div> */}
-      <Chat
+      </div>
+      {/* <Chat
         username={username}
         setUsername={setUsername}
         room={room}
         setRoom={setRoom}
         socket={socket}
-      />
+      /> */}
     </div>
   );
 }
