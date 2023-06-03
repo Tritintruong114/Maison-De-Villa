bf3f10da10fec27bd7f8681bb83d86f4c005ef0a
bruno <truongtritin.bee@gmail.com>
2023-06-03 22:58:54 +0700

    This is for Ai

--- src/pages/Ai/ImageGenerate.jsx
+++ src/pages/Ai/ImageGenerate.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import midJourney from "../../midjourney/config";
+
+import { useEffect } from "react";
 
 const ImageGenerate = () => {
+  const promt = "House 4k";
+
+  useEffect(() => {
+    console.log("Page Load start ");
+    midJourney(promt);
+    console.log("Page Load end");
+  }, []);
+
   return <div>ImageGenerate</div>;
 };
 
