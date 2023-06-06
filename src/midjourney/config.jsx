12418791864dc9679e033121326ae379775229e6
bruno <truongtritin.bee@gmail.com>
2023-06-06 23:03:36 +0700

    This is for fix UI with AI ImageGenerate

--- src/midjourney/config.jsx
+++ src/midjourney/config.jsx
@@ -1,36 +1,5 @@
-import axios from "axios";
-import { Midjourney } from "midjourney";
+// import axios from "axios";
+// import { Midjourney } from "midjourney";
 //npm
-
-const midJourney = async () => {
-  const client = new Midjourney({
-    ServerId: import.meta.env.VITE_PUBLIC_SERVER_ID,
-    ChannelId: import.meta.env.VITE_PUBLIC_CHANNEL_ID,
-    SalaiToken: import.meta.env.VITE_PUBLIC_SALAI_TOKEN,
-    Debug: true,
-    Ws: true,
-  });
-
-  const prompt = `house residence attractive interior designs --chaos 0 --quality 0.5 --ar 16:9`;
-
-  fetch(
-    "https://ai-custom-curriculum-production-cbad.up.railway.app/api/midjourneyAPI",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "text/plain",
-      },
-      body: prompt,
-    }
-  )
-    .then((response) => {
-      // Handle the response
-      console.log("Response:", response);
-    })
-    .catch((error) => {
-      // Handle any errors
-      console.error("Error:", error);
-    });
-};
 
 export default midJourney;
--- src/pages/Ai/ImageGenerate.jsx
+++ src/pages/Ai/ImageGenerate.jsx
@@ -1,20 +1,69 @@
-import midJourney from "../../midjourney/config";
+// import midJourney from "../../midjourney/config";
 
-import { useEffect } from "react";
+import { useState } from "react";
 
 const ImageGenerate = () => {
-  // const promt = "House 4k";
+  const [imgUri, setImgUri] = useState("");
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [inputPromt, setInpurPromt] = useState("");
+  const [showPromt, setShowPromt] = useState("");
 
-  useEffect(() => {
-    console.log("Page Load start ");
-    midJourney();
-    console.log("Page Load end");
-  }, []);
+  const midJourney = async () => {
+    const prompt = `residence attractive interior designs glass sunset full view luxury and have some people , with pool view, sky , cloud --chaos 0 --quality 0.5 --ar 16:9`;
+    fetch("https://toanjs.free.beeceptor.com/api/midjourneyAPI", {
+      method: "POST",
+      headers: {
+        "Content-Type": "text/plain",
+        tempoPassword: "truongtritin",
+      },
+      body: prompt,
+    })
+      .then((response) => {
+        setIsGenerating(true);
+        return response.json();
+      })
+      .then((data) => {
+        setIsGenerating(false);
+        console.log(data.uri);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  const handleGenerate = () => {
+    setShowPromt(inputPromt);
+    setInpurPromt("");
+    setIsGenerating(true);
+    // midJourney();
+  };
 
   return (
-    <div className="h-full w-full flex justify-center items-center  font-bold font-poppins ">
-      <div className="h-2/4 w-2/4 bg-white shadow-xl rounded-3xl flex justify-center items-center hover:scale-105 transition ease-in-out">
-        <h1 className="text-3xl sm:text-6xl from-black">Comming Soon</h1>
+    <div className="h-full w-full  flex justify-center items-center  font-bold font-poppins ">
+      <div className="h-full pt-24 gap-3 w-full flex-col flex items-center justify-center p-9">
+        <h1 className="text-3xl font-bold">Type your dream house</h1>
+        <div className="w-full gap-3 flex items-center justify-center">
+          <input
+            value={inputPromt}
+            onChange={(e) => setInpurPromt(e.target.value)}
+            className="w-full font-medium bg-fall bg-opacity-20 focus:outline-none px-6 py-3 rounded-3xl"
+          ></input>
+          <button
+            onClick={() => handleGenerate()}
+            className="font-medium bg-opacity-60 hover:bg-fall active:bg-lightBrown transition ease-in-out bg-darkBrown w-1/4 h-full rounded-full"
+          >
+            Generate
+          </button>
+        </div>
+        {isGenerating == true && (
+          <div className="w-full flex flex-col h-full pb-24">
+            <p className="w-full">{showPromt}</p>
+            <img
+              className="h-full w-full object-cover"
+              src="https://cdn.discordapp.com/attachments/1110473578283016223/1115642690701369424/rimn_house_residence_attractive_interior_designs_b13bbd38-1ebb-4171-bded-24d29705e331.png"
+            ></img>
+          </div>
+        )}
       </div>
     </div>
   );
